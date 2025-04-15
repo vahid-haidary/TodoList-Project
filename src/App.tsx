c25c9fcdefc3b7821580fc8b3338bdba6b145906
vahid-haidary <vahidhaidary0077@gmail.com>
2025-04-15 08:50:23 +0330

    login-page-final

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './components/Login'
-import Home from './components/Home'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
     <BrowserRouter>
     <Routes>
-      <Route element={<Home/>} path='/'/>
+      <Route element={<Home/>} path='/home'/>
       <Route element={<Login/>} path='/login'/>
+      <Route element={<Navigate to='/login'/>} path='/'  />
     </Routes>
     </BrowserRouter>
     </>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,29 +5,29 @@ import { FiMail } from "react-icons/fi";
 function Login() {
   return (
     <div className='w-full grid grid-cols-2 h-screen' >
-        <div className='w-full bg-black flex flex-col justify-center'>
-          <h1 className='text-white text-7xl font-semibold text-center'>Vahid-Todo</h1>
+        <div className='w-full bg-primary flex flex-col justify-center'>
+          <h1 className='text-white text-7xl font-semibold text-center '>Vahid-Todo</h1>
         </div>
           <div className='flex flex-col justify-center items-center h-screen' >
-            <form className='w-1/2 flex flex-col gap-3.5 *:bg-gray-100 *:text-gray-500 *:py-3 *:pl-3 *:rounded-md '>
+            <form className='w-1/2 flex flex-col gap-3.5 *:bg-gray-100 *:text-gray-500 *:py-3 *:pl-3 *:rounded-md *:focus-visible:outline-none '>
             <div className='mb-2 !bg-white !text-black'>
-              <h2 className='!font-bold !text-2xl' >Hello!</h2>
+              <h2 className='!font-bold !text-2xl ' >Hello!</h2>
               <span>Sign Up to Get Started</span>
             </div>
 
-            <div className='flex items-center'>
+            <div className='flex items-center *:focus-visible:outline-none'>
                 <FaUser className='mr-3' />
                 <input type="text" placeholder='Full Name'/>
             </div>
-            <div className='flex items-center'>
+            <div className='flex items-center *:focus-visible:outline-none'>
                 <FiMail className='mr-3' />
                 <input type="text" placeholder='Email Address'/>
             </div>
-            <div className='flex items-center'>
+            <div className='flex items-center *:focus-visible:outline-none'>
                 <FaLock className='mr-3' />
                 <input type="text" placeholder='Password'/>
             </div>
-                <button type='submit' className='!bg-black !text-white' >Register</button>
+                <button type='submit' className='!bg-primary !text-white'>Register</button>
             </form>
           </div>
     </div>
